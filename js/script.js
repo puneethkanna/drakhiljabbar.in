75309ebc9453ecc21582150c1d170d2ab3cc35f1
Sai Vadlakonda <sai2592004@gmail.com>
2026-04-26 14:30:55 +0530

    Fixed mobile responsiveness, hamburger menu and UI improvements

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-navMenu.classList.toggle("active");
+    hamburger.classList.toggle("active");   // for X animation
+    navMenu.classList.toggle("active");
 });
 
 document.querySelectorAll(".nav-menu a").forEach(link =>
-link.addEventListener("click", () => {
-navMenu.classList.remove("active");
-})
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    })
 );
